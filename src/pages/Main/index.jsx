7bb25fb3b8c5f928bbf57325be417bb83943c3e2
pedrolibarty <pedrolibarty@outlook.com>
2025-09-02 19:43:05 -0300

    feat: liberado

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -9,31 +9,31 @@ import { AdminContext } from "../../contexts/AdminContext";
 import Price from "../Price";
 
 const Main = () => {
-  const {getGifts, isModal, setGifts} = useContext(AdminContext)
+  const { getGifts, isModal, setGifts } = useContext(AdminContext);
 
   useEffect(() => {
-    getGifts()
+    getGifts();
 
     //setGifts([{name: "Cooktop de última geração", url_image: "cooktop-plastico.png", value: 5050, payvalue: 50, id: "011"}])
-  }, [])
+  }, []);
 
   return (
     <MainStyled>
-      {isModal && (<Price />)}
+      {isModal && <Price />}
       <Dashboard />
-      {/*<Description />
+      <Description />
       <Local />
       <List />
-      <Message />*/}
-      
-      <div className="add">
+      <Message />
+
+      {/*<div className="add">
         <h3>
           Nosso grande dia está se aproximando!
           <br/> Estamos preparando cada detalhe
           com muito carinho e, em breve, teremos novas atualizações por aqui!
           <br/>Um abraço, Lavínia e Gean ❤️
         </h3>
-      </div>
+      </div>*/}
     </MainStyled>
   );
 };
